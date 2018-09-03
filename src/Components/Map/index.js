0e53c7b8f69c5c7c463d5204b8b5f70eb3d6a277
Alex <alexanderlperez@gmail.com>
2018-09-03 15:49:48 -0400

    refactor

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-class Map extends Component {
-    // prevent unnecessary redraws
-    shouldComponentUpdate(nextProps) {
-        return Object.keys(this.props)
-            .some(key => this.props[key] !== nextProps[key]);
-    }
-
-    render() {
-        return (
-            <GoogleMap
-                defaultZoom={16}
-                center={this.props.coords}>
-                {this.props.isMarkerShown && <Marker position={this.props.coords} />}
-            </GoogleMap>
-        )
-    }
+function Map ({coords, isMarkerShown}) {
+    return (
+        <GoogleMap
+            defaultZoom={16}
+            center={coords}>
+            {isMarkerShown && <Marker position={coords} />}
+        </GoogleMap>
+    )
 }
 
 export default withGoogleMap(Map)
